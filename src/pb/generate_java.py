f3b18fd1eb258d50912f3976b8b656bcb3f9af2a
leiweibo <leiweibo@gmail.com>
2020-03-04 22:18:03 +0800

    初步完成生成rs和req的对象

--- src/pb/generate_java.py
+++ src/pb/generate_java.py
@@ -4,6 +4,9 @@ import re
 type_map = {"uint32": "String", "bytes": "String", "string": "String"}
 enum_type_map = {"MarketType": "market", "CurrencyType": "currency"}
 directory = 'pb_out'
+rs_config = {"generated_class_endfix": "RsData", "pb_file_prefix": "Ans", "sub_package": "res"}
+rq_config = {"generated_class_endfix": "RqData", "pb_file_prefix": "Req", "sub_package": "req"}
+normal_config = {"generated_class_endfix": "", "pb_file_prefix": "", "sub_package": ""}
 
 
 def generate_java(directory=directory):
@@ -15,15 +18,15 @@ def generate_java(directory=directory):
             files_in_dir = os.listdir(target_dir)
             # print(f'files in {target_dir}------------------')
             for file in files_in_dir:
-                if file.startswith('Ans'):
-                    parse_rs_pb(target_dir + "/" + file)
-                # elif file.startswith('Req'):
-                #     parse_rs_pb(target_dir + "/" + file, "Req", "RqData", "req")
+                if file.startswith(rs_config['pb_file_prefix']):
+                    parse_rs_pb(pb_file=target_dir + "/" + file, class_name=None, package_name=None, config=rs_config)
+                elif file.startswith(rq_config['pb_file_prefix']):
+                    parse_rs_pb(pb_file=target_dir + "/" + file, class_name=None, package_name=None, config=rq_config)
 
             print('\n')
 
 
-def parse_rs_pb(pb_file, class_name=None):
+def parse_rs_pb(pb_file, class_name=None, package_name=None, config={}):
     print('start to parse: ' + pb_file)
     import_lines = []
     file_content = ''
@@ -41,50 +44,57 @@ def parse_rs_pb(pb_file, class_name=None):
 
     if 'enum' in file_content:
         parse_enum_pb(pb_file, code_text)
-        return
+    else:
+        lines = code_text.split('\n')
+        # 没有msg_txt和msg_code的内容
+        content_without_msg_code = ""
+        names = re.compile(f'{config["pb_file_prefix"]}(.*?).proto').findall(pb_file)
+        class_content = ''
+        if not package_name:
+            package_name = pb_file.split("/")[-2].replace("_", "")
+        if not class_name and len(names):
+            class_name = names[0] + f"{config['generated_class_endfix']}"
+        for line in lines:
+            line = line.strip()
 
-    lines = code_text.split('\n')
-    # 没有msg_txt和msg_code的内容
-    content_without_msg_code = ""
-    names = re.compile(f'Ans(.*?).proto').findall(pb_file)
-    class_content = ''
-    if not class_name and len(names):
-        class_name = names[0] + f"RsData"
-    for line in lines:
-        line = line.strip()
-
-        if ('msg_code' in line) or ('msg_text' in line) or line.strip() == '':
-            pass
-        else:
-            content_without_msg_code += (line+'\n')
-            declared_field = re.compile('([A-Za-z0-9_*]+)\s').findall(line)
-            if len(declared_field) >= 3:
-                type = declared_field[1]
-                name = declared_field[2]
-                if type in type_map:
-                    class_content += f'  @JSONField(name="{name.upper()}")\n'
-                    class_content += f'  private {type_map[type]} {name}; \n\n'
-                else:
-                    if type in enum_type_map:
+            if ('msg_code' in line) or ('msg_text' in line) or line.strip() == '':
+                pass
+            else:
+                content_without_msg_code += (line+'\n')
+                declared_field = re.compile('([A-Za-z0-9_*]+)\s').findall(line)
+                if len(declared_field) >= 3:
+                    type = declared_field[1]
+                    name = declared_field[2]
+                    if type in type_map:
                         class_content += f'  @JSONField(name="{name.upper()}")\n'
-                        class_content += f'  private String {enum_type_map[type]}; \n\n'
-                    for import_line in import_lines:
-                        if type in import_line:
-                            imported_pb = re.compile('import \"(.*?)\"').findall(import_line)[0]
-                            parse_rs_pb(directory + "/" + imported_pb, class_name=class_name)
-    if class_content == '':
-        return
-    print(content_without_msg_code)
+                        class_content += f'  private {type_map[type]} {name}; \n\n'
+                    else:
+                        if type in enum_type_map:
+                            class_content += f'  @JSONField(name="{name.upper()}")\n'
+                            class_content += f'  private String {enum_type_map[type]}; \n\n'
+                        for import_line in import_lines:
+                            if type in import_line:
+                                imported_pb = re.compile('import \"(.*?)\"').findall(import_line)[0]
+                                specify_config_str = class_name if class_name else imported_pb
+                                if rs_config['pb_file_prefix'] in specify_config_str or rs_config['generated_class_endfix'] in specify_config_str:
+                                    parse_rs_pb(pb_file=directory + "/" + imported_pb, class_name=class_name, package_name=package_name, config=rs_config)
+                                elif rq_config['pb_file_prefix'] in specify_config_str or rq_config['generated_class_endfix'] in specify_config_str:
+                                    parse_rs_pb(pb_file=directory + "/" + imported_pb, class_name=class_name, package_name=package_name, config=rq_config)
+                                else:
+                                    parse_rs_pb(pb_file=directory + "/" + imported_pb, class_name=class_name, package_name=package_name, config=normal_config)
 
-    package_name = pb_file.split("/")[-2].replace("_", "")
-    content_package = f'package com.niubang.trade.tth.share.model.{package_name}.res;\n\n'
-    content_import = 'import com.alibaba.fastjson.annotation.JSONField;\nimport com.niubang.common.ToString;\nimport lombok.Data;\n\n'
-    content_start = f'@Data \npublic class {class_name} extends ToString ' + "{\n\n"
-    content_body = class_content
-    content_end = "}"
-    content = (content_package + content_import + content_start + content_body + content_end)
-    generate_java_file(file_name=class_name, sub_dir=package_name + "/" + "res", content=content)
-    print('\n')
+        if class_content == '':
+            return
+        print(content_without_msg_code)
+
+        content_package = f'package com.niubang.trade.tth.share.model.{package_name}.{config["sub_package"]};\n\n'
+        content_import = 'import com.alibaba.fastjson.annotation.JSONField;\nimport com.niubang.common.ToString;\nimport lombok.Data;\n\n'
+        content_start = f'@Data \npublic class {class_name} extends ToString ' + "{\n\n"
+        content_body = class_content
+        content_end = "}"
+        content = (content_package + content_import + content_start + content_body + content_end)
+        generate_java_file(file_name=class_name, sub_dir=package_name + '/' + f'{config["sub_package"]}', content=content)
+        print('\n')
 
 
 def parse_enum_pb(pb_file, code_text):
@@ -126,7 +136,6 @@ def generate_java_file(out_put='java_out', sub_dir="", file_name="", content="")
     with open(final_path + "/" + file_name, "w+") as f:
         f.write(content)
 
-
 if __name__ == '__main__':
     generate_java()
-    # parse_rs_pb('pb_out/trade_bank_biz/AnsQryHisTransfer.proto')
+    # parse_rs_pb(pb_file='pb_out/trade_apply_biz/AnsQryApplyEnableMarket.proto', class_name=None, config=rs_config)
