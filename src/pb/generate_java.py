c7b16801abdc9dab4d8f0dc9a0f62c2397f7f6e2
leiweibo <leiweibo@gmail.com>
2020-03-09 20:28:17 +0800

    对生成的对象的基类的属性进行过滤

--- src/pb/generate_java.py
+++ src/pb/generate_java.py
@@ -82,6 +82,9 @@ def parse_rs_pb(pb_file, class_name=None, package_name=None, config={}, repeated
                             name = f.strip()
                             break
 
+                    if name in base_properties:
+                        continue
+
                     if type in type_map:
                         if name == 'user_pwd':
                             class_content += f'  private {type_map[type]} {process_name(name)}; \n\n'
@@ -328,7 +331,7 @@ def generate_convert(class_name, code_content_lines, repeat, raw_import_pb_file,
                     content_body += f'        builder.set{name}(StringUtil.stringToBytes(rq.get{name}()));\n'
                     import_items.append('import com.niubang.trade.tth.biz.manager.util.StringUtil;')
                 elif type in enum_type_map:
-                    content_body += f'        builder.set{enum_type_map[type].title()}({type}.valueOf(TypeUtils.ObjectToInt(rq.get{name}())));\n'
+                    content_body += f'        builder.set{enum_type_map[type].title()}({type}.forNumber(TypeUtils.ObjectToInt(rq.get{name}())));\n'
                     import_items.append('import com.niubang.common.util.TypeUtils;')
                     import_items.append(f'import com.niubang.trade.tth.biz.manager.dataobject.tradebasedefine.NB{type}.{type};')
                 else:
