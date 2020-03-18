8b2081e55d9b874ee0cd647a8f46ceaa40d9c32a
leiweibo <leiweibo@gmail.com>
2020-03-18 11:45:58 +0800

    本地shell脚本里面添加一个copy操作

--- src/pb/start.sh
+++ src/pb/start.sh
@@ -7,4 +7,12 @@ cd pb_out
 cd ..
 python3 generate_java.py
 
+cd pb_java
+# 以下的地址临时写死
+cp -r com/* /Users/weibolei/Dev/nb-workspace/trade-tcp-http/biz/manager/src/main/java/com
+cd ../java_out
+cp -r convert/* /Users/weibolei/Dev/nb-workspace/trade-tcp-http/biz/manager/src/main/java/com/niubang/trade/tth/biz/manager/convert
+
+cp -r models/* /Users/weibolei/Dev/nb-workspace/trade-tcp-http/share/model/src/main/java/com/niubang/trade/tth/share/model
+
 echo '处理完成'
